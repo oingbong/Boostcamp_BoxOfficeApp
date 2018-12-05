cc03affed77eb22bf1f96377d9b26d4a323ef62b
oingbong <oingbong@gmail.com>
2018-12-05 14:32:35 +0900

    Configure Structure of View

---
+++ BoxOfficeApp/BoxOfficeApp/CollectionViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CollectionViewController.swift
+//  BoxOfficeApp
+//
+//  Created by oingbong on 05/12/2018.
+//  Copyright © 2018 oingbong. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
