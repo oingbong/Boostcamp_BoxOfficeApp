1ca37823faf1ef553f2b9c62bd964792489cc637
oingbong <oingbong@gmail.com>
2018-12-08 16:52:17 +0900

    Add ActionSheet and sorted func in tableview

--- BoxOfficeApp/BoxOfficeApp/TableViewController.swift
+++ BoxOfficeApp/BoxOfficeApp/TableViewController.swift
@@ -19,8 +19,13 @@ class TableViewController: UIViewController {
     }
     
     private func configure() {
+        parse(orderType: 0)
+        appendButtonItem()
+    }
+    
+    private func parse(orderType: Int) {
         // 1. URL 객체 정의 - else 일 때 사용자에게 알람 (선택사항)
-        guard let url = URL(string: "http://connect-boxoffice.run.goorm.io/movies?order_type=1") else { return }
+        guard let url = URL(string: "http://connect-boxoffice.run.goorm.io/movies?order_type=\(orderType)") else { return }
         // 2. URLRequest 객체 정의 및 요청 내용
         var request = URLRequest(url: url)
         request.httpMethod = "GET"
@@ -48,6 +53,31 @@ class TableViewController: UIViewController {
         
         // 5. GET 전송
         task.resume()
+    }
+    
+    private func appendButtonItem() {
+        guard let image = UIImage(named: "ic_settings") else { return }
+        let buttonItem = UIBarButtonItem(image: image, style: .plain, target: self, action: #selector(sorted))
+        buttonItem.tintColor = UIColor.white
+        self.navigationItem.rightBarButtonItem = buttonItem
+    }
+    
+    @objc private func sorted() {
+        let alert = UIAlertController(title: "정렬방식 선택", message: "영화를 어떤 순서로 정렬할까요?", preferredStyle: .actionSheet)
+        let reservationRate = UIAlertAction(title: "예매율", style: .default) { (_) in
+            self.parse(orderType: 0)
+        }
+        let curation = UIAlertAction(title: "큐레이션", style: .default) { (_) in
+            self.parse(orderType: 1)
+        }
+        let date = UIAlertAction(title: "개봉일", style: .default) { (_) in
+            self.parse(orderType: 2)
+        }
+        
+        alert.addAction(reservationRate)
+        alert.addAction(curation)
+        alert.addAction(date)
+        self.present(alert, animated: true, completion: nil)
     }
 }
 
