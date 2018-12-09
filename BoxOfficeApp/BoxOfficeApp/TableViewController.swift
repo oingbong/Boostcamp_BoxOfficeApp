abc4bb3db84abe15d1760afa1a741667b850bc20
oingbong <oingbong@gmail.com>
2018-12-09 15:28:59 +0900

    Add Parser and Using in Tableview

--- BoxOfficeApp/BoxOfficeApp/TableViewController.swift
+++ BoxOfficeApp/BoxOfficeApp/TableViewController.swift
@@ -25,35 +25,12 @@ class TableViewController: UIViewController {
     }
     
     private func parse(orderType: Int) {
-        // 1. URL 객체 정의 - else 일 때 사용자에게 알람 (선택사항)
-        guard let url = URL(string: "http://connect-boxoffice.run.goorm.io/movies?order_type=\(orderType)") else { return }
-        // 2. URLRequest 객체 정의 및 요청 내용
-        var request = URLRequest(url: url)
-        request.httpMethod = "GET"
-        // 3. HTTP 메세지 헤더 설정
-        
-        // 4. URLSession 객체로 전송 및 응답값 처리 로직 작성
-        let task = URLSession.shared.dataTask(with: request) { data, response, error in
-            // 4-1. 서버가 응답이 없거나 통신이 실패했을 때
-            if let err = error {
-                print(err.localizedDescription)
-                return
-            }
-            
-            do {
-                guard let responseData = data else { return }
-                let items = try JSONDecoder().decode(Movies.self, from: responseData)
-                self.movies = Movies(orderType: items.orderType, data: items.data)
-                DispatchQueue.main.async {
-                    self.tableview.reloadData()
-                }
-            } catch {
-                print("error")
+        Parser.json(with: orderType) { (items) in
+            self.movies = Movies(orderType: items.orderType, data: items.data)
+            DispatchQueue.main.async {
+                self.tableview.reloadData()
             }
         }
-        
-        // 5. GET 전송
-        task.resume()
     }
     
     private func appendButtonItem() {
@@ -104,3 +81,4 @@ extension TableViewController: UITableViewDataSource {
         return cell
     }
 }
+
