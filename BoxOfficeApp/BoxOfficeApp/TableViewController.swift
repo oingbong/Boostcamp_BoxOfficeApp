ce9d4fb86def54f50a1460c71cbee86730d89c36
oingbong <oingbong@gmail.com>
2018-12-09 15:10:17 +0900

    Add SortName Enum for titleBar & connect titlebar and rightButton click

--- BoxOfficeApp/BoxOfficeApp/TableViewController.swift
+++ BoxOfficeApp/BoxOfficeApp/TableViewController.swift
@@ -20,6 +20,7 @@ class TableViewController: UIViewController {
     
     private func configure() {
         parse(orderType: 0)
+        configureTitle(from: 0)
         appendButtonItem()
     }
     
@@ -62,16 +63,26 @@ class TableViewController: UIViewController {
         self.navigationItem.rightBarButtonItem = buttonItem
     }
     
+    private func configureTitle(from element: Int) {
+        guard let sort = SortName(rawValue: element) else { return }
+        self.navigationItem.title = sort.description
+        let textAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+        self.navigationController?.navigationBar.titleTextAttributes = textAttributes
+    }
+    
     @objc private func sorted() {
         let alert = UIAlertController(title: "정렬방식 선택", message: "영화를 어떤 순서로 정렬할까요?", preferredStyle: .actionSheet)
         let reservationRate = UIAlertAction(title: "예매율", style: .default) { (_) in
             self.parse(orderType: 0)
+            self.configureTitle(from: 0)
         }
         let curation = UIAlertAction(title: "큐레이션", style: .default) { (_) in
             self.parse(orderType: 1)
+            self.configureTitle(from: 1)
         }
         let date = UIAlertAction(title: "개봉일", style: .default) { (_) in
             self.parse(orderType: 2)
+            self.configureTitle(from: 2)
         }
         
         alert.addAction(reservationRate)
