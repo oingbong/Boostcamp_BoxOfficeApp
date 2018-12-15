0b19ad736d7e5839b571fa44901a6e6b14a4c72f
oingbong <oingbong@gmail.com>
2018-12-15 14:24:41 +0900

    Edit Grouping Project & Add DetailVC, each Cell and simple configure infoCell

---
+++ BoxOfficeApp/BoxOfficeApp/DetailCell/CommentCell.swift
@@ -0,0 +1,24 @@
+//
+//  CommentCell.swift
+//  BoxOfficeApp
+//
+//  Created by oingbong on 15/12/2018.
+//  Copyright © 2018 oingbong. All rights reserved.
+//
+
+import UIKit
+
+class CommentCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ BoxOfficeApp/BoxOfficeApp/DetailCell/InfoCell.swift
@@ -0,0 +1,24 @@
+//
+//  InfoCell.swift
+//  BoxOfficeApp
+//
+//  Created by oingbong on 15/12/2018.
+//  Copyright © 2018 oingbong. All rights reserved.
+//
+
+import UIKit
+
+class InfoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
